080117aad04f6219f71a40cac088d20f60ac8297
abhishekalcanzar <abhishek.bajpai@alcanzarsoft.com>
2015-07-07 19:25:33 +0530

    Update config.js

--- master/config/config.js
+++ master/config/config.js
@@ -7,77 +7,7 @@ module.exports = {
 
 	bootstrap: function(bootstrap_cb) {
 		if(bootstrap_cb) bootstrap_cb();
-		// async.parallel([
-
-		// 	function (cb) {
-
-		// 		// If default administrator already exists, get out
-		// 		Account.find({
-		// 			where: {email: 'admin@olympus.io'}
-		// 		}).done(function (err, account) {
-		// 			if (err) throw err;
-		// 			if (account) return cb && cb();
-
-		// 			// Otherwise create a new administrator account
-		// 			Account.create({
-		// 				name: 'Administrator',
-		// 				title: 'Administrator',
-		// 				email: 'admin@olympus.io',
-		// 				password: 'abc123',
-		// 				isAdmin: true,
-		// 				verified: true,
-		// 				verificationCode: null,
-		// 				avatar_fname: null,
-		// 				avatar_mimetype: null
-		// 			}).done(function done (err, account) {
-		// 				if (err) throw err;
-
-		// 				// Now create a workgroup, assigning the new account as an admin
-		// 				Directory.createWorkgroup({
-		// 					name: 'Sample Workgroup',
-		// 					accountId: account.id
-		// 				}, function(err, results) {
-		// 					if (err) throw err;
-
-		// 					cb && cb();
-		// 				});
-		// 			});
-		// 		});
-		// 	},
-
-		// 	function (cb) {
-		// 		// Also create a default API app developer
-		// 		Developer.find({
-		// 			where: {api_key: '3y6gp1hz9de7cgvkn7xqjb3285p8udf2'}
-		// 		}).done(function (err, developer) {
-		// 			if (err) throw err;
-		// 			if (developer) return cb && cb();
-
-		// 			// Otherwise create a new administrator account
-		// 			Developer.create({
-		// 				api_key: '3y6gp1hz9de7cgvkn7xqjb3285p8udf2',
-		// 				api_secret: 'ctDv8bIUmdJtChHP357xJ1ZspKh32rwq',
-		// 				app_name: 'Test API App',
-		// 				redirect_url: 'http://www.pigandcow.com/olympus_test_api_app'
-		// 			}).done(function done (err, developer) {
-		// 				if (err) throw err;
-		// 				else return cb && cb();
-		// 			});
-
-		// 			AccountDeveloper.create({
-		// 				api_key: '3y6gp1hz9de7cgvkn7xqjb3285p8udf2',
-		// 				account_id: 1,
-		// 				code: '',
-		// 				access_token: 'baudzVitCraHCB1',
-		// 				refresh_token: 'abcdefg',
-		// 				code_expires: '2020-01-01',
-		// 				access_expires: '2020-01-01',
-		// 				refresh_expires: '2020-01-01',
-		// 				scope: 3
-		// 			});
-		// 		});
-		// 	}
-		// ], function(err, results) {bootstrap_cb && bootstrap_cb();});
+	
 	},
 
 	// File store adapter configuration
@@ -86,38 +16,30 @@ module.exports = {
 		// Which adapter to use
 		adapter: 's3',
 
-		// Amazon S3 API credentials
-	/*	s3: {
-           accessKeyId: 'AKIAIUPRTVZFV3GJR6DA',
-           secretAccessKey: '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',
-           bucket: 'app.olympus.io',
-           region: 'US_EAST_1'
-       },*/
-
 	s3: {
 		
-		accessKeyId: 'AKIAIUPRTVZFV3GJR6DA',
-		secretAccessKey: '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',
-		bucket: 'app.olympus.io',
-		region: 'US_EAST_1'
+		accessKeyId: 'AWS_ACCESS_KEY_ID',
+		secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+		bucket: 'AWS_BUCKET',
+		region: 'AWS_REGION'
 	},
 
 
 		// OpenStack Swift API credentials
 		swift: {
-			host: '209.99.54.131',
-			port: '8080',
-			serviceHash: 'AUTH_cba4c29df381471682041b859ce4d8ae',
-			container: 'olympus_main'
+			host: 'SWIFT_HOST',
+			port: 'SWIFT_PORT',
+			serviceHash: 'SWIFT_HASH',
+			container: 'SWIFT_CONTAINER'
 		},
 
 		// Keystone API credentials
 		keystone: {
-			host:		'209.99.54.131',
-			port:		'5000',
-			tenant:		'demo', // tenant === "project" in Horizon dashboard
-			username:	'admin',
-			password:	'truvan1x'
+			host:		'KEYSTONE_HOST',
+			port:		'KEYSTONE_PORT',
+			tenant:		'KEYSTONE_TENANT', // tenant === "project" in Horizon dashboard
+			username:	'KEYSTONE_USERNAME',
+			password:	'KEYSTONE_PASSWORD'
 		}
 	},
 
@@ -189,21 +111,4 @@ module.exports = {
     // what determines this config options and implement that.
     privateDeployment: false
 
-		/*
-	// Future:
-	riggingSequence: {
-	    "mobile": ['./public/dependencies', './public/mobile-ui'],
-	    "desktop": ['./public/dependencies', './public/desktop-ui']
-	}
-	*/
-
-	/*
-	// To use HTTPS, just include a key and cert, for example:
-	ssl: {
-		key: fs.readFileSync('ssl/private.key.pem'),
-		cert: fs.readFileSync('ssl/combined.crt')
-	};
-	*/
-
-	// Override with any local configuration
 };
