fc75676a8da575e9f9ba5e0f1f5694be3b4160f8
Asspl <abhishek.bajpai@alcanzarsoft.com>
2015-09-01 17:46:01 +0530

    changes in api file and images that are responsible for theme customization

--- master/api/models/AccountDeveloper.js
+++ master/api/models/AccountDeveloper.js
@@ -1,15 +1,15 @@
 AccountDeveloper = Model.extend({
   tableName: 'accountdeveloper',
 
-	api_key: STRING,
-	account_id: INTEGER,
-	code: STRING,
-	access_token: STRING,
-	refresh_token: STRING,
-	code_expires: DATE,
-	access_expires: DATE,
-	refresh_expires: DATE,
-	scope: INTEGER
+	api_key 		: STRING,
+	account_id 		: INTEGER,
+	code 			: STRING,
+	access_token	: STRING,
+	refresh_token 	: STRING,
+	code_expires 	: DATE,
+	access_expires 	: DATE,
+	refresh_expires : DATE,
+	scope 			: INTEGER
 });
 
 AccountDeveloper.SCOPE_READ = 1;
