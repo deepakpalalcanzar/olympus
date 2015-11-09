1f1901f4448fe2cefd1637cb24ce5d09e9f5e894
Asspl <abhishek.bajpai@alcanzarsoft.com>
2015-11-09 15:36:16 +0530

    master files

---
+++ master/mast/components/SettingsComponent.js
@@ -0,0 +1,14 @@
+Mast.components.SettingsComponent  = Mast.Component.extend({
+	
+	template: '.settings-template',
+	outlet: '#content'
+	// events: {
+	// 	'click .setting-save-button' : 'saveCompanyInfo'
+	// },
+
+	// saveCompanyInfo : function(){
+	// 	alert("aaaaaaaaaaaaaaa");
+	// }
+
+
+});
